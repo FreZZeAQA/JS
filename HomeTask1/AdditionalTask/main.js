970f0760a47561c58d915a0d9286787e13b56074
Yura <yuras92@ukr.net>
2023-11-27 13:01:59 +0200

    add and rename additional tasks

--- HomeTask1/AdditionalTask/main.js
+++
@@ -1,7 +0,0 @@
-let userName = prompt("Write your name:");
-let userSurname =prompt("Write your surname:");
-let userAge = parseInt(prompt("Write your age:"));
-
-console.log(userName)
-console.log(userSurname)
-console.log(userAge)
