73e8230ffb3dae3f0de8f434daa8800a6d9b9ba5
Yura <yuras92@ukr.net>
2023-11-28 12:23:29 +0200

    add home tasks from lesson three

---
+++ HomeTask3/Task1/main.js
@@ -0,0 +1,20 @@
+//- За допомогою циклу for і document.write() вивести 10 блоків div
+// c довільним текстом всередині і індексом всередині
+
+for (let i = 0; i < 10; i++) {
+    document.write(`<div> ${i} Hello OktenWeb</div>`)
+}
+
+//- За допомогою циклу while вивести в документ 20 блоків h1
+// c довільним текстом і індексом всередині.
+
+
+let i = 0;
+while (i < 20) {
+    document.write(`<h1> ${i} Hello OktenWeb</h1>`)
+    i++;
+}
+
+
+
+
